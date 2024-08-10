1a1e81f3c72f4d53c90ab9346df0b8d35a67ae6a
Jose Cruz <resoj.cruz@gmail.com>
2024-08-10 04:25:45 -0400

    Add main driver file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 
 require('./style.css');
 
-// Import modules
-const Player = require('./player.js');
-const Gameboard = require('./gameboard.js');
-const Ship = require('./ship.js');
+const UIController = require('./uicontroller');
 
-const playerOne = new Player();
-const playerTwo = new Player();
+const uiController = new UIController();
 
-const playerOneGameboard = playerOne.gameboard;
-const playerTwoGameboard = playerTwo.gameboard;
-
-//Set Ships up
-let index = 0;
-
-playerOneGameboard.ships.forEach(ship => {
-    playerOneGameboard.placeShip(ship, index, index);
-    // playerTwoGameboard.placeShip(ship, index, index);
-    index += 1;
-})
-
-
-const playerOneBoardArea = document.getElementById('player-one-board-area');
-
-playerOneGameboard.gameboard.forEach(gridX => {
-    gridX.forEach(gridY => {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        if(gridY instanceof Ship) {
-            gridCell.style.backgroundColor = 'gray';
-            gridCell.textContent = gridY.name;
-        }
-        playerOneBoardArea.appendChild(gridCell);
-    })
-})
+uiController.controller();
